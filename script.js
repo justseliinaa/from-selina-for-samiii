292fa22041f2094f822ac801c636e25cd76dc6b0
justseliinaa <159721820+justseliinaa@users.noreply.github.com>
2024-02-11 21:20:59 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "YAY CYA ON VALENTINES DAY MWUAH♡";
+  gif.src =
+    "https://i.pinimg.com/originals/41/37/52/413752be3dd3b9f3c1bd6561646a99e4.gif";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
+
